095bc6c754d04063f2183ab0352da8de4341a2e6
oneilj57 <james.oneill57@mail.dcu.ie>
2021-03-30 15:05:54 +0100

    application update for homepage

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './App.css';
+import Navbar from './components/Navbar'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import './App.css'
+import Home from './components/pages/Home'
+
+function App() {
+  return (
+      <React.Fragment>
+        <Router>
+            <Navbar/>
+            <Switch>
+                <Route path='/' exact component={Home}/>
+            </Switch>
+        </Router>
+      </React.Fragment>
+  )
+}
+
+export default App;
