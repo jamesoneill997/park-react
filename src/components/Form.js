f0f47f15581e776e50caafd267a36d46c5f63780
oneilj57 <james.oneill57@mail.dcu.ie>
2021-03-31 20:55:54 +0100

    added communication with API to sign up form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,35 +1,67 @@
 import React from 'react'
 import './Form.css'
 
-
 export const Form =() => {
+
+
+        const firstNameEl = React.useRef(null);
+        const surnameEl = React.useRef(null);
+        const emailEl = React.useRef(null);
+        const passwordEl = React.useRef(null);
+        const typeEl = React.useRef(null);
+        
+        const handleSubmit = e => {
+            e.preventDefault();
+        
+            const data = {
+            firstName: firstNameEl.current.value,
+            surname: surnameEl.current.value,
+            type: typeEl.current.value,
+            email: emailEl.current.value,
+            password: passwordEl.current.value
+            }
+            
+            fetch('https://parkapiv0.herokuapp.com/users', {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                },
+            
+                body: JSON.stringify(data)
+            }).then(function(response){
+                return response.json();
+            })
+            
+        }
+    
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
                 <h3>Sign Up</h3>
 
                 <div className="form-group">
                     <label>First name</label>
-                    <input type="text" className="form-control" placeholder="First name" />
+                    <input type="text" className="form-control" placeholder="First name" ref={firstNameEl}/>
                 </div>
 
                 <div className="form-group">
                     <label>Last name</label>
-                    <input type="text" className="form-control" placeholder="Last name" />
+                    <input type="text" className="form-control" placeholder="Last name" ref={surnameEl}/>
                 </div>
 
                 <div className="form-group">
                     <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
+                    <input type="email" className="form-control" placeholder="Enter email" ref={emailEl}/>
                 </div>
 
                 <div className="form-group">
                     <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
+                    <input type="password" className="form-control" placeholder="Enter password" ref={passwordEl}/>
                 </div>
 
                 <div className="form-group">
                     <label>I am a...</label>
-                    <select name="type" id="type">
+                    <select name="type" id="type" ref={typeEl}>
                         <option disabled selected value> -- select an option -- </option>
                         <option value="admin">Car Owner</option>
                         <option value="user">Carpark Owner</option>
@@ -47,3 +79,5 @@ export const Form =() => {
             </form>
     )
 }
+
+
